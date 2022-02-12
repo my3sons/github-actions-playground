170e0d0f37f48e8b78643a9e1d84855fe183120d
my3sons <my3sons@users.noreply.github.com>
2022-02-12 07:39:07 -0600

    added more stuff to action

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -8472,15 +8472,25 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
 try {
+
+  core.debug("debug message");
+  core.warning("my warning messsage");
+  core.error("my error message");
+
+
+
   //  throw(new error("oh my that is not good"));
     const name = core.getInput('who-to-greet');
     
+    core.setSecret(name); //this will cause name to appear as *** in logs
     console.log(`Hello ${name}`);
     
     const time = new Date();
     core.setOutput("time", time.toTimeString());
     
     console.log(JSON.stringify(github, null, '\t'));
+
+    core.exportVariable("HELLO", "hello Carey"); //this create env variable that can be used in subsequent steps
 
 } catch (error) {
     core.setFailed("it failed!");
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,15 +2,25 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+
+  core.debug("debug message");
+  core.warning("my warning messsage");
+  core.error("my error message");
+
+
+
   //  throw(new error("oh my that is not good"));
     const name = core.getInput('who-to-greet');
     
+    core.setSecret(name); //this will cause name to appear as *** in logs
     console.log(`Hello ${name}`);
     
     const time = new Date();
     core.setOutput("time", time.toTimeString());
     
     console.log(JSON.stringify(github, null, '\t'));
+
+    core.exportVariable("HELLO", "hello Carey"); //this create env variable that can be used in subsequent steps
 
 } catch (error) {
     core.setFailed("it failed!");
